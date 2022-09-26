9819945aa841ab910d0252ac473c22c84337f238
lutfi-haslab <work.lutfiikbalmajid@gmail.com>
2022-09-26 14:27:53 +0700

    updates env

--- config/db.go
+++ config/db.go
@@ -2,19 +2,27 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	// go-sql-driver/mysql
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 // SetupDB : initializing mysql database
 func SetupDB() *gorm.DB {
-	USER := "root"
-	PASS := "IoA6Ei5fzqwZkYfxXx6a"
-	HOST := "containers-us-west-74.railway.app"
-	PORT := "7373"
-	DBNAME := "railway"
+	errEnv := godotenv.Load("./.env")
+	if errEnv != nil {
+		fmt.Printf("Could not load .env file")
+		os.Exit(1)
+	}
+
+	USER := os.Getenv("USER")
+	PASS := os.Getenv("PASS")
+	HOST := os.Getenv("HOST")
+	PORT := os.Getenv("PORT")
+	DBNAME := os.Getenv("DBNAME")
 	
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
 	db, err := gorm.Open("mysql", URL)
